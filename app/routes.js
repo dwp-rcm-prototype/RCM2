13493fa08ee91358dc577e4113f4378950d3bd89
Rob Eijkman <websites@eijkman.com>
2015-03-17 15:03:15 +0000

    First set of pages: welcome, simple form and complete

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,35 @@
+
 module.exports = {
   bind : function (app) {
+
+    app.use(function(req, res, next) {
+      if (req.path.substr(-1) == '/' && req.path.length > 1) {
+        var query = req.url.slice(req.path.length);
+        res.redirect(301, req.path.slice(0, -1) + query);
+      } else {
+        next();
+      }
+    });
 
     app.get('/', function (req, res) {
       res.render('index');
     });
 
     // add your routes here
+    app.get('/rcm2', function (req, res) {
+      res.render('rcm/index');
+    });
+    app.get('/rcm2/dummy', function (req, res) {
+      res.render('rcm/dummy');
+    });
+    app.get('/rcm2/form', function (req, res) {
+      res.render('rcm/form');
+    });
+    app.post('/rcm2/complete', function (req, res) {
+      res.render('rcm/complete');
+    });
 
   }
 };
+
+
