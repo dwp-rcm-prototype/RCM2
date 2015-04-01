e19b314a407b18c2ccdee497fff20426ba75915b
Rob Eijkman <websites@eijkman.com>
2015-04-01 12:04:00 +0100

    fix in progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,9 +62,9 @@
 
 
             var msg = form.find('div.validation-message').get(0);
-            //if (msg) {
-            //    $(msg).remove();
-            //}
+            if (msg != null) {
+                $(msg).remove();
+            }
 
             // loop each validation-group
 /*
